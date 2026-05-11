b2934732bc653f7e1a7e74484167edda4b9bc512
RAJANNITCS <rajan.nit.cs@gmail.com>
2026-05-11 19:15:07 +0530

    add sorting

---
+++ 3.Sorting/BubbleSortExample.java
@@ -0,0 +1,15 @@
+// WAP to demostrating Bubble Sort
+
+public class BubbleSortExample {
+
+    // Method for Bubble Sort
+    static void bubbleSort(int[] arr) {
+
+        // 
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
